55c7b25d4d6120881bea4ccbc89faab599e73dc1
SpinCraft95 <74097042+SpinCraft95@users.noreply.github.com>
2021-07-27 12:46:29 +0200

    V1.0.1

---
+++ terminal V1.0.1.bat
@@ -0,0 +1,27 @@
+:load
+@echo off
+cls
+cd C:\Windows\System32
+title PowerTerminal
+echo Microsoft Windows [version 10.0.19042.1110]
+echo (c) Microsoft Corporation. Tous droits reserves.
+echo Terminal By SpinCraft95
+:run
+set "command="
+echo.
+echo __
+cd
+echo "%computername%|%username%|%date%|%time%"
+set /p "command=>>>"
+echo.
+if "%command%"=="reload" goto reload
+if "%command%"=="" goto run
+if "%command%"=="exit" exit
+if "%command%"=="quit" exit
+if "%command%"=="stop" exit
+if not "%command%"=="" cmd /c %command%
+goto run
+
+:reload
+cls
+goto load
